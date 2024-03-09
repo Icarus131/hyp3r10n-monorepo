00373d1d05b79c7f422f74540ace11df6453f919
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2024-03-09 12:05:06 +0530

    timescale setup

--- server/index.js
+++ server/index.js
@@ -9,6 +9,15 @@ const app = express();
 const port = 8000;
 const server = http.createServer(app);
 const io = socketIo(server);
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  password: "password",
+  host: "127.0.0.1",
+  port: 5432, // default Postgres port
+  database: "postgres",
+});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,10 +27,47 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
-app.get("/", (req, res) => {
-  res.status(200).send("Sup?");
-});
 
-app.listen(8000, () => {
-  console.log(`Server is running on port`);
-});
+// Create table query
+const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS raw_logs_ingested (
+        log_id SERIAL,
+        time TIMESTAMPTZ NOT NULL,
+        level VARCHAR(255),
+        message TEXT,
+        resourceId VARCHAR(255),
+        traceId VARCHAR(255),
+        spanId VARCHAR(255),
+        commit VARCHAR(255),
+        metadata VARCHAR(255),
+        PRIMARY KEY(log_id, time)
+    );
+`;
+
+// Create hypertable query
+const createHypertableQuery = `
+    SELECT create_hypertable('raw_logs_ingested', 'time', if_not_exists => TRUE);
+`;
+
+// Create index queries
+const createIndexQueries = [
+  `CREATE INDEX IF NOT EXISTS idx_level ON raw_logs_ingested (level);`,
+  `CREATE INDEX IF NOT EXISTS idx_message_fulltext ON raw_logs_ingested USING GIN(to_tsvector('english', message));`,
+  `CREATE INDEX IF NOT EXISTS idx_resourceId ON raw_logs_ingested (resourceId);`,
+];
+
+// Initialize database schema
+pool
+  .query(createTableQuery)
+  .then(() => pool.query(createHypertableQuery))
+  .then(() => Promise.all(createIndexQueries.map((query) => pool.query(query))))
+  .then(() => console.log("Database setup complete"))
+  .catch((error) => console.error("Error setting up database:", error));
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("Sup?");
+// });
+
+// app.listen(8000, () => {
+//   console.log(`Server is running on port`);
+// });
