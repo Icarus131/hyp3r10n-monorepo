003d4374e8df8a9e6c75f70e6f56574525c22a36
Icarus131 <anirudhprasad131@gmail.com>
2025-01-07 15:43:06 +0530

    feat: PES1UG22CS094 - dummy commit for uni lab assignment

--- client/src/components/LandingPageComponent.jsx
+++ client/src/components/LandingPageComponent.jsx
@@ -1,89 +1,91 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const LandingPageComponent = () => {
-	return (
-		<div className="h-[100vh] w-[100vw] bg-black bg-grid-white/[0.2] relative flex flex-col items-center justify-center">
-			<div
-				style={{
-					background:
-					"radial-gradient(circle at center, rgba(0,0,0,0) 0%, rgba(0,0,0,0.7) 30%, rgba(0,0,0,1) 100%)",
-				}}
-				className="absolute pointer-events-none inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
-			></div>
-			<div className="flex flex-col items-center justify-center px-5">
-				<p className="text-6xl md:text-8xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-3">
-					Spectra.
-				</p>
-				<p className="text-neutral-500 max-w-xl mx-auto  text-sm md:text-lg text-center relative z-10">
-					Your end-to-end cryptocurrency tracker. A descriptive currency tracker using the wallet ID. Get graphical representation, realtime monitoring of wallets, mail in case of suspicious activity and finally, export transaction csv file locally to your system.
-				</p>
-				<div className="flex flex-row gap-4 items-center justify-center">
-					<Link to="/app">
-						<div className="flex flex-row items-center justify-center gap-4 p-2 m-2">
-							<button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
-								<span className="absolute inset-0 overflow-hidden rounded-full">
-									<span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
-								</span>
-								<div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-									<span className="m-1 md:m-2">Proceed</span>
-									<svg
-										width="16"
-										height="16"
-										viewBox="0 0 24 24"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											stroke="currentColor"
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth="1.5"
-											d="M10.75 8.75L14.25 12L10.75 15.25"
-										></path>
-									</svg>
-								</div>
-								<span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
-							</button>
-						</div>
-					</Link>
+  return (
+    <div className="h-[100vh] w-[100vw] bg-black bg-grid-white/[0.2] relative flex flex-col items-center justify-center">
+      <div
+        style={{
+          background:
+            "radial-gradient(circle at center, rgba(0,0,0,0) 0%, rgba(0,0,0,0.7) 30%, rgba(0,0,0,1) 100%)",
+        }}
+        className="absolute pointer-events-none inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+      ></div>
+      <div className="flex flex-col items-center justify-center px-5">
+        <p className="text-6xl md:text-8xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-500 py-3">
+          Spectra.
+        </p>
+        <p className="text-neutral-500 max-w-xl mx-auto  text-sm md:text-lg text-center relative z-10">
+          Your end-to-end cryptocurrency tracker. A descriptive currency tracker
+          using the wallet ID. Get graphical representation, realtime monitoring
+          of wallets, mail in case of suspicious activity and finally, export
+          transaction csv file locally to your system.
+        </p>
+        <div className="flex flex-row gap-4 items-center justify-center">
+          <Link to="/app">
+            <div className="flex flex-row items-center justify-center gap-4 p-2 m-2">
+              <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+                <span className="absolute inset-0 overflow-hidden rounded-full">
+                  <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+                </span>
+                <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                  <span className="m-1 md:m-2">Proceed</span>
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.5"
+                      d="M10.75 8.75L14.25 12L10.75 15.25"
+                    ></path>
+                  </svg>
+                </div>
+                <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+              </button>
+            </div>
+          </Link>
 
-					<a href="https://www.canva.com/design/DAF-aep-ar0/ZLe59HShcFDL-T-F-rLIBA/edit" className="">
-						<div className="flex flex-row items-center justify-center gap-4 p-2 m-2">
-							<button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
-								<span className="absolute inset-0 overflow-hidden rounded-full">
-									<span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
-								</span>
-								<div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-									<span className="m-1 md:m-2">Know More</span>
-									<svg
-										width="16"
-										height="16"
-										viewBox="0 0 24 24"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											stroke="currentColor"
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth="1.5"
-											d="M10.75 8.75L14.25 12L10.75 15.25"
-										></path>
-									</svg>
-								</div>
-								<span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
-							</button>
-						</div>
+          <a
+            href="https://www.canva.com/design/DAF-aep-ar0/ZLe59HShcFDL-T-F-rLIBA/edit"
+            className=""
+          >
+            <div className="flex flex-row items-center justify-center gap-4 p-2 m-2">
+              <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+                <span className="absolute inset-0 overflow-hidden rounded-full">
+                  <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+                </span>
+                <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                  <span className="m-1 md:m-2">Know More</span>
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.5"
+                      d="M10.75 8.75L14.25 12L10.75 15.25"
+                    ></path>
+                  </svg>
+                </div>
+                <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+              </button>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-					</a>
-				</div>
-
-			</div>
-		</div>
-
-
-	)
-}
-
-export default LandingPageComponent
+export default LandingPageComponent;
